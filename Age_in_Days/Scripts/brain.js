257577edbf28ae02d92eab9ef425ba956a3dd6ad
Dev-jr-8 <devhereforcoding@gmail.com>
2022-08-30 22:56:17 +0530

    Brain(JS) Added

--- Age_in_Days/Scripts/brain.js
+++ Age_in_Days/Scripts/brain.js
@@ -1,1 +1,17 @@
-alert("Sucess")
+//To print the age of user in days
+
+function AgeinDays() {
+   var birthyear = prompt("Tell me your Birth Year : ");
+
+   var ageindays = (2022 - birthyear) * 365;
+
+   var h1 = document.createElement("h1");
+
+   var textAnswer = document.createTextNode("You are" + ageindays + " days old....");
+
+   h1.setAttribute("id", "ageindays");
+
+   h1.appendChild(textAnswer);
+
+   document.getElementById("flex-box-result").appendChild(h1);
+}
