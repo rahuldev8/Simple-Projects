f8453bb18543e0929900ee99f9bf133d547e86a6
Dev-jr-8 <devhereforcoding@gmail.com>
2022-08-31 21:55:29 +0530

    brain updated

--- Simple_Calculator/brain.js
+++ Simple_Calculator/brain.js
@@ -56,22 +56,7 @@ class Calculator {
       this.previousOperand = "";
    }
 
-   getDisplayNumber(number) {
-      const stringNumber = number.toString();
-      const integerDigits = parseFloat(stringNumber.split(".")[0]);
-      const decimalDigits = stringNumber.split(".")[1];
-      let integerDisplay;
-      if (isNaN(integerDigits)) {
-         integerDisplay = "";
-      } else {
-         integerDisplay = integerDigits.toLocaleString("en", { maximumFractionDigits: 0 });
-      }
-      if (decimalDigits != null) {
-         return `${integerDisplay}.${decimalDigits}`;
-      } else {
-         return integerDisplay;
-      }
-   }
+
 
    updateDisplay() {
       this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
