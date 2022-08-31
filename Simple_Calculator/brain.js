a639ef37aa7eff081679a6924cae3eeb4b4250da
Dev-jr-8 <devhereforcoding@gmail.com>
2022-08-31 21:55:46 +0530

    barin updated

--- Simple_Calculator/brain.js
+++ Simple_Calculator/brain.js
@@ -68,6 +68,23 @@ class Calculator {
    }
 }
 
+getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+       integerDisplay = "";
+    } else {
+       integerDisplay = integerDigits.toLocaleString("en", { maximumFractionDigits: 0 });
+    }
+    if (decimalDigits != null) {
+       return `${integerDisplay}.${decimalDigits}`;
+    } else {
+       return integerDisplay;
+    }
+ }
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
